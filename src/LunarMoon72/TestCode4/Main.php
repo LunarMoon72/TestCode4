e249e5c308e24f3448e6563ba8fb2271bf17e197
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-08-06 11:24:50 -0400

    Create Main.php

---
+++ src/LunarMoon72/TestCode4/Main.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace LunarMoon72\TestCode4;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\Command;
+
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase{
+	public function onEnabled(){
+		$this->getLogger()->info("Plugin has been enabled");
+	}
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+		switch($cmd->getName()){
+			case "perm"
+			  if($player->hasPermission("testcode4.cmd")){
+			  	if($sender instanceof Player){
+			  		$sender->sendMessage("This command only works for players!");
+			  	}else{
+			  		if(!isset($args[0]) or (is_int($args[0]) and $args[0] > 0)){
+			  			$args[0] = 4;
+			  		}
+			  	    $sender->getInventory()->addItem(Item::get(1, 0, $args[0])); 
+			  	    $sender->sendMessage("You have recived" .count $args[0].);
+			  	}   
+                    
+			  }
+		}     break;
+	}
+}   return true;
